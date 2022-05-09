8c58fb9df3e4ce35d935a7c95139dea4de89d32a
Jorge luis Montoya <31372667+koke07@users.noreply.github.com>
2022-05-08 19:00:32 -0500

    feat: some operations and exercise about areas

--- src/main.go
+++ src/main.go
@@ -11,7 +11,7 @@ func main() {
 
 	// declaracion de variable enteras
 
-	base := 22
+	/*base := 22
 	var altura int = 182
 	var peso int
 
@@ -24,16 +24,68 @@ func main() {
 	var c string
 	var d bool
 
-	fmt.Println(a, b, c, d)
+	fmt.Println(a, b, c, d)*/
 
 	// ejercicio el cuadrado de un numero
 
-	const baseCuadrado = 50
+	/*const baseCuadrado = 50
 	area := baseCuadrado * baseCuadrado
-	fmt.Println(" el area del cuadro es:", area)
+	fmt.Println(" el area del cuadro es:", area)*/
 
 	/*const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 
 	fmt.Println("El área del cuadrado es:", areaCuadrado)*/
+
+	// operators in goland
+
+	x := 10
+	y := 20
+	//suma
+	suma := x + y
+	fmt.Println(suma)
+	//resta
+	resta := x - y
+	fmt.Println(resta)
+	//multiplicacion
+	mult := x * y
+	fmt.Println(mult)
+	// division
+	div := x / y
+	fmt.Println(div)
+	// modulo
+	mod := x % y
+	fmt.Println(mod)
+	//incrementar
+	x++
+	fmt.Println(x)
+	//decrementar
+	x--
+	fmt.Println(x)
+
+	// Rectángulo
+	baseRectangulo := 20
+	alturaRectangulo := 10
+
+	areaRectangulo := baseRectangulo * alturaRectangulo
+
+	fmt.Println("El Area del Rectángulo es :", areaRectangulo)
+
+	// Circulo : AreaCirculo = pi por radio al cudrado
+	const PI float64 = 3.14 // Constant
+	var radioCirculo float64 = 10
+
+	areaCirculo := PI * radioCirculo * radioCirculo
+
+	fmt.Println("El Area del Circulo es :", areaCirculo)
+
+	// Trapecio
+	var baseUno float64 = 6
+	var baseDos float64 = 15
+	var alturaTrapecio float64 = 25
+
+	areaTrapecio := ((baseUno + baseDos) * alturaTrapecio) / 2
+
+	fmt.Println("El Area del Trapecio es :", areaTrapecio)
+
 }
